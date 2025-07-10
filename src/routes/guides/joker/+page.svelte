8c25627d044d58ca81a4cdb12616407b64367b7d
Arsene <persona5@gmail.com>
2025-07-10 21:01:33 +0200

    fix: made bigger best teams character images

--- src/routes/guides/joker/+page.svelte
+++ src/routes/guides/joker/+page.svelte
@@ -33,8 +33,8 @@
 
   /* portraits – réduits de 50 % */
   .teamRow img{
-    width:150px;
-    height:150px;
+    width:200px;
+    height:200px;
     object-fit:contain;
     border:2px solid transparent;
     border-radius:.75rem;
